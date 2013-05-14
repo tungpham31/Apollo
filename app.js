9a48275ed3fe72ccbbd16ca666939d55f54ca72d
Tung Pham <tungpham31@gmail.com>
2013-05-14 01:43:15 -0400

    Primitive Implementation of Synchronized Streaming

--- app.js
+++ app.js
@@ -32,6 +32,67 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+//###The correct way to host an HTTP server using an Express instance
+var httpServer = http.createServer(app);
+console.log("Express server hosting on port " + app.get("port") + ".");
+httpServer.listen(app.get("port"));
+
+// Setup WebSocket.
+var io = require('socket.io').listen(httpServer);
+
+//###Register 'connection' event - which is when a client becomes connected to the server.
+//		This is used for all of our websocket stuff.
+io.sockets.on('connection', function(socket){
+	
+	//register room join event; called by client after connection initiated.
+	socket.on('join', function(dataForRoom){ 
+		var theRoom = dataForRoom.room;
+		socket.join(theRoom); //just takes a string for which room to join
+	});
+
+	//Used when a new user registers.
+	socket.on('addUser', function(data){
+		var theUser = data.userName; //pull the username from the JSON object
+		io.sockets.in('login').emit('newUser', {"userName" : theUser});
+	});
+
+	//Used when chat messages are sent (in the Chat page)
+	socket.on('addChatMessage', function(messageObject){
+		var theMessage = messageObject.message;
+		var theUserName = messageObject.username;
+		var broadcasterName = theUserName;
+		io.sockets.in(broadcasterName).emit('newHangoutMessage', {"message" : theMessage, "username" : theUserName })
+	});
+
+	//Used when chat messages are sent in the hangout page
+	socket.on('addHangoutMessage', function(messageObject){
+		var theMessage = messageObject.message;
+		var theUserName = messageObject.username;
+		var broadcasterName = messageObject.broadcasterName;
+		io.sockets.in(broadcasterName).emit('newHangoutMessage', {"message" : theMessage, "username" : theUserName})	
+	});
+
+	//Used when video state messages are sent in the hangout page
+	socket.on('youtube_player_state_change', function(messageObject){
+		var theMessage = messageObject.message;
+		var broadcasterName = messageObject.username;
+		io.sockets.in(broadcasterName).emit('youtube_player_state_change', {"message" : theMessage, "username" : broadcasterName})	
+	});
+
+	//Used when a new video is added in the chat page
+	socket.on('add_new_video_to_youtube_player', function(messageObject){
+		var url = messageObject.url;
+		var broadcasterName = messageObject.username;
+		io.sockets.in(broadcasterName).emit('add_new_video_to_youtube_player', {"url" : url, "username" : broadcasterName})	
+	});
+
+	//Used when receiving an update from broadcaster's youtube player
+	socket.on('update_youtube_player_state', function(messageObject){
+		var videoId = messageObject.videoId;
+		var currentTime = messageObject.current_time;
+		var playerState = messageObject.player_state;
+		var broadcasterName = messageObject.username;
+		console.log("app receive: " + videoId + " " + currentTime + " " + playerState + " " + broadcasterName);
+		io.sockets.in(broadcasterName).emit('update_youtube_player_state', {"videoId" : videoId, "currentTime" : currentTime, "playerState" : playerState});	
+	});
 });
