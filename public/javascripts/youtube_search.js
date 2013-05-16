aacdcff42f628b8c2e31e6c5962a727ba7ada6fc
Tung Pham <tungpham31@gmail.com>
2013-05-16 01:28:27 -0400

    Search Box Implemented

--- public/javascripts/youtube_search.js
+++ public/javascripts/youtube_search.js
@@ -1,26 +1,37 @@
+$(document).ready(function() {
+ 	document.getElementById("search_form").style.visibility="hidden";
+
+ 	$('#search_form').submit(function(event){
+ 		event.preventDefault();
+		// Search for a given string.
+		console.log("In Search");
+		console.log($('#search_input').val());
+		var q = $('#search_input').val();
+		var request = gapi.client.youtube.search.list({
+			q: q,
+			part: 'snippet',
+			maxResults: '1'
+		});
+
+		request.execute(function(response) {
+		var str = JSON.stringify(response.result);
+		var videoId = response.result.items[0].id.videoId;
+		setTimeout(function(){player.loadVideoById(videoId, 0, "default");}, 2000);
+		console.log("result = " + str);
+		});
+	});
+});
+
 var apiKey = 'AIzaSyBa4zviAvx76SlSq7cWBV5nU09K3FVWnxs';
 
 function handleClientLoad() {
-	// Step 2: Reference the API key
-	console.log("handle client load");
+	// Reference the API key.
 	gapi.client.setApiKey(apiKey);
-	console.log("after provide key");
-	gapi.client.load('youtube', 'v3', search);
+	// Load youtube service
+	gapi.client.load('youtube', 'v3', onYouTubeServiceLoaded);
 }
 
-// Search for a given string.
-function search() {
-	var q = "payphone";
-	var request = gapi.client.youtube.search.list({
-		q: q,
-		part: 'snippet',
-		maxResults: '1'
-	});
-
-	request.execute(function(response) {
-	var str = JSON.stringify(response.result);
-	var videoId = response.result.items[0].id.videoId;
-	setTimeout(function(){player.loadVideoById(videoId, 0, "default");}, 2000);
-	console.log("result = " + videoId);
-	});
+function onYouTubeServiceLoaded(){
+	// Set search form visible so that user can search for songs
+	document.getElementById("search_form").style.visibility="visible";
 }
