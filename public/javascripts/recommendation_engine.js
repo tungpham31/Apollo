2bf039eacef143f4bd3c5df37a15c1dce1cb8f2c
Tung Pham <tungpham31@gmail.com>
2013-06-18 21:54:20 -0400

    New Fix

--- public/javascripts/recommendation_engine.js
+++ public/javascripts/recommendation_engine.js
@@ -1,9 +1,7 @@
 var echo_nest_api_key = "7VI2RLSXC4GBPF6S7";
-var create_radio_request = "http://developer.echonest.com/api/v4/playlist/dynamic/create?api_key=7VI2RLSXC4GBPF6S7&type=catalog-radio&seed_catalog=CACAJLI13ED4A7FB2C&adventurousness=.9";
+var create_radio_request = "http://developer.echonest.com/api/v4/playlist/dynamic/create?api_key=7VI2RLSXC4GBPF6S7&type=catalog-radio&seed_catalog=CACAJLI13ED4A7FB2C&adventurousness=.8";
 var next_song_request = "http://developer.echonest.com/api/v4/playlist/dynamic/next?api_key=" + echo_nest_api_key + "&format=json&session_id="
-var session_id = "236a7f3a9aaa46e48449708184b87b0f";
-var favorite_song_request = "http://developer.echonest.com/api/v4/playlist/dynamic/feedback?api_key=7VI2RLSXC4GBPF6S7&format=json&session_id=" + session_id + "&favorite_song=last";
-var ban_song_request = "http://developer.echonest.com/api/v4/playlist/dynamic/feedback?api_key=7VI2RLSXC4GBPF6S7&format=json&session_id=" + session_id + "&ban_song=last";
+var session_id; // = "236a7f3a9aaa46e48449708184b87b0f";
 var lastRecommendedSong = undefined;
 var lastRecommendedSongPositionInPlaylist = undefined;
 
@@ -12,7 +10,7 @@ createRadio();
 function createRadio(){
 	$.getJSON(create_radio_request + "&_=" + Math.floor(Math.random()*1000000), function(data) {
 		if (data.response.status.message === "Success"){
-			//session_id = data.response.session_id;
+			session_id = data.response.session_id;
 			next_song_request = next_song_request + session_id;
 		}
 	});
@@ -35,6 +33,7 @@ function getRecommendedSong(){
 
 // Send feedback back to Echonest indicating that user likes a song.
 function likeSong(){
+	var favorite_song_request = "http://developer.echonest.com/api/v4/playlist/dynamic/feedback?api_key=7VI2RLSXC4GBPF6S7&format=json&session_id=" + session_id + "&favorite_song=last";
 	$.getJSON(favorite_song_request + "&_=" + Math.floor(Math.random()*1000000), function(data) {
 		if (data.response.status.message === "Success"){
 			console.log("Like successufully");
@@ -45,6 +44,7 @@ function likeSong(){
 
 // Send feedback back to Echonest indicating that user dislikes a song.
 function dislikeSong(){
+	var ban_song_request = "http://developer.echonest.com/api/v4/playlist/dynamic/feedback?api_key=7VI2RLSXC4GBPF6S7&format=json&session_id=" + session_id + "&ban_song=last";
 	$.getJSON(ban_song_request + "&_=" + Math.floor(Math.random()*1000000), function(data) {
 		if (data.response.status.message === "Success"){
 			console.log("Dislike successufully");
